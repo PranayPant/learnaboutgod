68b9f122172e729f37421476177f576b4e9b8b5d
PranayPant <ppantg1@gmail.com>
2024-06-30 16:22:23 -0500

    init

---
+++ app/is-god-real/page.tsx
@@ -0,0 +1,122 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Is God Real",
+  description:
+    "Find out why God is real. If you have asked yourself the question if there is a God, this is for you. You will find proof of God here!",
+};
+
+export default function IsGodReal() {
+  return (
+    <div>
+      <h1>Is God Real?</h1>
+      <article>
+        Many have asked this question, or some variation of this question.
+        Although there are some who will refuse to believe in God even if he was
+        standing in front of them, many of us have some degree of faith that
+        there is a higher power that controls all of creation. Yet, we are all
+        still plagued by doubts and questions regarding His existence. These
+        doubts are a product of our ignorance - we can only see the physical
+        reality around us, but not the person who created it. To slay these
+        weeds of doubt, we need the sword of divine wisdom so that the flower of
+        Faith may bloom in full.
+      </article>
+      <h2>What is the proof that God exists?</h2>
+      <article>
+        There are three main ways to know something and prove its existence.
+        <ul>
+          <li>
+            <strong>Proof by Observation</strong>
+            <p>
+              We can use the five knowledge acquiring senses (sight, sound,
+              smell, taste, touch) to gather information about the world. These
+              senses are designed to give feedback about external stimuli, from
+              which we can make logical inferences about our environment. For
+              example, if you are in a dark room and cannot see, you can still
+              use your sense of touch to tell where the furniture is. If your
+              phone goes off, you can hear where it is and infer where your bed
+              must be, and so on.
+            </p>
+            <p>
+              However, this way of knowing is imperfect. One example is that of
+              a mirage - many times when driving on a hot day, we see what
+              appears to be a puddle of water on the road, but it quickly
+              disappears as we get near that spot. Sometimes we cannot make out
+              where exactly a sound is coming from, and we mistake it to be
+              coming from teh attic, say, instead of the garage or even outside
+              the house. This is due to the fact that our senses are imperfect
+              and our mind, which processes the data that our senses gather, is
+              also imperfect. Even the most accurate machines do output
+              incorrect data from time to time. Therefore, not being able to see
+              or hear God in no way proves that He is not real. Forturantely,
+              there is another way to know.
+            </p>
+          </li>
+          <li>
+            <strong>Proof by Logical Inference</strong>
+            <p>
+              If you saw a puddle on teh highway in the middle of July in Texas,
+              you mighgt doubt if that is real, due to the fact that it is too
+              hot for a small puddle to stay and not evaporate. This is an
+              example of logical infernece. Even though our eyes are seeing one
+              thing, our logical inference says something opposite. Another
+              example would be inferring the prescence of fire upon seeing
+              smoke. We have not actually seen the smoke, but we know from basic
+              sense that smoke can only be created by fire, so there would have
+              to be fire somewhere closeby. This is a powerful method by which
+              we can understand the world around us, including God. There is one
+              way that is even more powerful, however, and will be essential for
+              us to have complete knowledge of God.
+            </p>
+          </li>
+          <li>
+            <strong>Proof by Citation</strong>
+            <p>
+              Our ability to infer is limited by our knowledge and expertise in
+              a particular field. This is why scientists often change theur
+              views after several decades, because by then there is new
+              knowledge that undermines the inferneces made by the previous
+              generation. Ultimately, since we did not create all of creation,
+              we are not the best equipped to know how everything works.
+            </p>
+            <p>
+              Imagine trying to fix a car without a service manual. Having to
+              learn everything by using the ascending process of learning (e.g.
+              the scientific method) would be very time consuming and lead to
+              more faulures than we could ever tolerate. By consulting a trusted
+              authority like the manufacturer provided service manual, we are
+              able to save a lot of time and get the results we want. The key
+              here is that the autority is knowledgeable and the student trusts
+              said authority.
+            </p>
+          </li>
+        </ul>
+      </article>
+      <h2>Putting it all together</h2>
+      <article>
+        Let us know go back to the main question: Is God Real? To answer this
+        question, we will first use logical inference.
+        <p>
+          Yes, God is real. It takes an intelligent being to create something
+          intelligent, the most obvious example being the human body. To say He
+          is not real is to say things create themselves, which is absurd. If we
+          say a five course meal set up on a table in our house but no physical
+          sign of anyone being there, we would still never assume that the mean
+          manifested by itself. Perhaps your friends are surpsiing you, perhaps
+          your Mom came from out of town and decided to cook for but had to
+          leave right away...literally anything is more plausible except that
+          idea that it manifested itself.
+        </p>
+        <p>
+          Another super obvious way to infer God is to realize the death is
+          real. What happens at death? The physical, insentient body is
+          separated from the subtle, sentient Soul. In order to understand this,
+          we have to first understand that we are not our bodies, we are the
+          conscousness inhabitting our bodies. Once we accpet that, accepting
+          that God is the one reesponsible for orchestrating life and death
+          comes naturally.
+        </p>
+      </article>
+    </div>
+  );
+}
